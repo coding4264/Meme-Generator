f0a152766ff1ee4d66e00c4ff5cfd5e1d9a18da7
coding4264 <119458259+coding4264@users.noreply.github.com>
2023-01-04 09:39:49 -0500

    Add files via upload

---
+++ func.js
@@ -0,0 +1,22 @@
+const memeButton = document.querySelector(".memes .meme-button");
+const memeImage = document.querySelector(".memes img");
+const memeTitle = document.querySelector(".memes .meme-title");
+const memeAuthor = document.querySelector(".memes .meme-author")
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+
+}
+
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes").then((response) => response.json()).then(data => {
+        updateDetails(data.url, data.title, data.author);
+    })
+}
+
+memeButton.addEventListener("click", generateMeme)
+
+generateMeme();
