323fdaf2e020c9daf7b34b73c30d3e10f061d461
coding4264 <119458259+coding4264@users.noreply.github.com>
2023-04-22 08:23:16 -0700

    Turning meme generator into YouTube shorts generator

--- func.js
+++ func.js
@@ -20,3 +20,24 @@ const generateMeme = () => {
 memeButton.addEventListener("click", generateMeme)
 
 generateMeme();
+
+function capture() {
+  const captureElement = document.querySelector('#capture')
+  html2canvas(captureElement)
+    .then(canvas => {
+      canvas.style.display = 'none'
+      document.body.appendChild(canvas)
+      return canvas
+    })
+    .then(canvas => {
+      const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+      const a = document.createElement('a')
+      a.setAttribute('download', 'my-image.png')
+      a.setAttribute('href', image)
+      a.click()
+      canvas.remove()
+    })
+}
+
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', capture)
